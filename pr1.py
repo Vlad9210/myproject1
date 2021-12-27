0c8a31b3dd373a959ef45ddda6f80c0460657bbb
Vlad9210 <75477730+Vlad9210@users.noreply.github.com>
2021-12-27 14:57:36 +0100

    Create pr1.py

---
+++ pr1.py
@@ -0,0 +1,22 @@
+def Zyczenia():
+    name = input("Podaj imie");
+    wishes = input("Napisz życzenia");
+    print("Witaj " + name + " Życzę " + wishes)
+
+def TakeInputFromUser(): 
+    return (float(input("Podaj liczbę")));
+    
+def suma(number1, number2):
+    return (number1 + number2);
+    
+    
+#Zyczenia()
+#print(suma(TakeInputFromUser(), TakeInputFromUser()))
+
+def buduj_zdanie(info):
+    a = info + " Jest zaletą funkcji"
+    return (a)
+
+info = "ght"
+i = buduj_zdanie(info);
+print(i);
