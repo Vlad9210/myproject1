fa7fdf8393f4d25812b96c2c9caa183b5eb067ee
Vlad9210 <75477730+Vlad9210@users.noreply.github.com>
2022-01-13 16:45:03 +0100

    Add files via upload

---
+++ operatory matematyczne.py
@@ -0,0 +1,21 @@
+print((2 + 2) * 2)
+
+#Dzielenie
+print(5/2)
+print(5//2) #jeśli chcemy tylko liczby całkowite
+
+#Mnożenie
+print(2*3)
+print(2 ** 4) #podwójna gwiazdka to do potęgi
+
+#Powiekszenie zmiennej
+x = 5 
+x = x + 1
+print(x)
+x += 1  # += 1 powiększenie o 1
+print(x)
+
+#konwersja typów
+a = input("podaj 1 liczbę: ")
+b = input("podaj 2 liczbę ")
+print(int(a) + int(b)) #dodajemy "int" aby zsumowac liczby , bez by było łaczenie a nie dodawanie
