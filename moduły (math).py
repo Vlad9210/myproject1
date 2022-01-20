0b83789393efc24851ee2b8218cd2eb085cc65fb
Vlad9210 <75477730+Vlad9210@users.noreply.github.com>
2022-01-20 16:14:19 +0100

    Add files via upload

---
+++ moduły (math).py
@@ -0,0 +1,11 @@
+import random
+print(random.randint(1, 10)) #randint zwraca nam wartości z podanego przedziału
+
+from random import randint
+print(randint(1, 10))
+
+from math import pi #Math - moduł związany z róznymi obliczeniami matematycznymi
+print(pi) #pi - stała wartość pi
+
+from math import sqrp as pierwiastek #sqrt - zwracanie pierwiastka od jakiejś wartości
+print(pierwiastek(16))
