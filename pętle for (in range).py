6984a9caef99dbebfa1753ef953d3dd7a089239e
Vlad9210 <75477730+Vlad9210@users.noreply.github.com>
2022-09-07 14:29:57 +0200

    Add files via upload

---
+++ pętle for (in range).py
@@ -0,0 +1,42 @@
+name = ["Ala", "Basia", "Kasia"]
+for name in name:
+    print ("Następne imie z listy " + name)
+    
+    
+for i in range (5):
+    print (i)
+    
+    
+for i in range (3):
+    print (f"zostało {3 - i} soki")
+    print ("glup glup glup")
+    
+print ("Nie ma juz soków")
+
+for i in range (1, 3):
+    print (i)
+    
+for i in range (1, 8, 2):
+    print (i)
+    
+for i in range (10, 0, -2):
+    print (i)
+    
+    
+    
+for i in range (5):
+    print (i)
+    
+    
+for i in range (5, 10):
+    print (i)
+    
+    
+for i in range (5, 30, 4):
+    print (i)
+    
+for i in range (10, 5, -1):
+    print (i)
+    
+for i in range (20, 0, -2):
+    print (i)
